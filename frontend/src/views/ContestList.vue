43b1cc5d13ba0b16bc46da337f1d6d97af1cbd4b
Quoc-Khang Tran <quockhang@192.168.2.28>
2025-10-26 01:44:14 +0700

    fix: remove trophy icon

--- frontend/src/views/ContestList.vue
+++ frontend/src/views/ContestList.vue
@@ -201,14 +201,9 @@ onRouteQueryUpdate(fetch)
             <td class="contest-title">
               <Button type="text" class="table-button" @click="visit(item)">
                 <span class="button-text">{{ item.title }}</span>
-                <Poptip
-                  trigger="hover"
-                  :content="`This contest is of type ${item.option.type}`" placement="top"
-                >
-                  <Tag :color="item.option.type === CType.OI ? 'green' : 'blue'" class="contest-mark">
-                    <span class="pi pi-trophy"></span>&nbsp;{{ item.option.type }}
-                  </Tag>
-                </Poptip>
+                <Tag :color="item.option.type === CType.OI ? 'green' : 'blue'" class="contest-mark">
+                  {{ item.option.type === CType.OI ? 'OI' : 'ICPC' }}
+                </Tag>
                 <Poptip
                   v-show="item.status === status.Reserve" trigger="hover"
                   content="This item is reserved, no one could see this, except admin" placement="top"
